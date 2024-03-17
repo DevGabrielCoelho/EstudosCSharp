8363988829f48aa527f00c1eeefb2923152c2363
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-17 07:31:42 -0300

    estudos

---
+++ CursoDeLogicaCSharp/ExercicioMatriz1/ExercicioMatriz1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExercicioMatriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] quantidade = Console.ReadLine().Split(' ');
+            int num1 = int.Parse(quantidade[0]);
+            int num2 = int.Parse(quantidade[1]);
+            int[,] matrix = new int[num1, num2];
+
+            for (int i = 0; i < num1; i++)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+                for (int o = 0; o < num2; o++)
+                {
+                    matrix[i, o] = int.Parse(entrada[o]);
+                }
+            }
+
+            Console.WriteLine("NUMEROS NEGATIVOS:");
+            foreach (int negativos in matrix) if (negativos < 0) Console.WriteLine(negativos);
+
+        }
+    }
+}
