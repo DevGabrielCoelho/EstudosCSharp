47df0e0fd3893ff8ae8e34d76a5fbe0a42cec289
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-17 07:50:43 -0300

    estudos

---
+++ CursoDeLogicaCSharp/ExercicioMatriz2/ExercicioMatriz2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioMatriz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[quantidade, quantidade];
+            int[] soma = new int[quantidade];
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+                for (int o = 0; o < quantidade; o++)
+                {
+                    matrix[i, o] = int.Parse(entrada[o]);
+                    soma[i] += matrix[i, o];
+                }
+            }
+
+            foreach (int x in soma) Console.WriteLine(x);
+
+        }
+    }
+}
