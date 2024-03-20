b15cb46ca933020f245bb7ddaf4a8613f71c0e35
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-20 17:00:54 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj1/ExercicioClassObj1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassObj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Insira a Largura e Altura do retângulo");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(retangulo);
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj1/ExercicioClassObj1/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassObj1
+{
+    class Retangulo
+    {
+        public double Largura, 
+                      Altura;
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+        public override string ToString()
+        {
+            return $"AREA = {Area().ToString("f2", CultureInfo.InvariantCulture)}\n" +
+                   $"PERÍMETRO = {Perimetro().ToString("f2", CultureInfo.InvariantCulture)}\n" +
+                   $"DIAGONAL = {Diagonal().ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
