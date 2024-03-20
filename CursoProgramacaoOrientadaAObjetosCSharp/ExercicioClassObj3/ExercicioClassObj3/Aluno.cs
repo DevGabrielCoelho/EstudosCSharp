8e6e398d1c3db2d48a88d9eca54d495dd4cf1e1a
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-20 18:26:57 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj3/ExercicioClassObj3/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassObj3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double PrimeiraNota, SegundaNota, TerceiraNota;
+
+        public double NotaFinal()
+        {
+            return PrimeiraNota + SegundaNota + TerceiraNota;
+        }
+
+        public override string ToString()
+        {
+            if (NotaFinal() >= 60.00)
+            {
+                return $"{NotaFinal().ToString("f2", CultureInfo.InvariantCulture)}\n" +
+                       $"APROVADO";
+            }
+            else
+            {
+                return $"{NotaFinal().ToString("f2", CultureInfo.InvariantCulture)}\n" +
+                       $"REPROVADO\n" +
+                       $"FALTARAM {(60.00 - NotaFinal()).ToString("f2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj3/ExercicioClassObj3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioClassObj3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            aluno.PrimeiraNota = double.Parse(Console.ReadLine());
+            aluno.SegundaNota = double.Parse(Console.ReadLine());
+            aluno.TerceiraNota = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTA = " + aluno);
+        }
+    }
+}
