9f1f8700a11bbcf7e44a177da67ce62d14712d03
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-20 17:21:53 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj2/ExercicioClassObj2/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ExercicioClassObj2
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto, Imposto;
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto / 100.00 * porcentagem; 
+        }
+        public override string ToString()
+        {
+            return $"{Nome}, ${SalarioLiquido().ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioClassObj2/ExercicioClassObj2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassObj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"\nFuncionário: {funcionario}\n\n" +
+                              $"Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine($"\nDados atualizados: {funcionario}");
+        }
+    }
+}
