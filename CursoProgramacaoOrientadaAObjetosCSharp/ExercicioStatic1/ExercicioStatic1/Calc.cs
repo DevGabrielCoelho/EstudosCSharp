6bd8f63c99fd173d03e297a99087cdc4d0bd3a38
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-21 11:59:18 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioStatic1/ExercicioStatic1/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioStatic1
+{
+    class Calc
+    {
+        public static double Calcular(double dollar, double quantidade) 
+        {
+            return (dollar + (dollar/100.0*6.0)) * quantidade;
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioStatic1/ExercicioStatic1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioStatic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você quer comprar? ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); double reais = Calc.Calcular(cotacao, compra);
+            Console.WriteLine($"Valor a ser pago em reais: {Calc.Calcular(cotacao, compra).ToString("f2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
