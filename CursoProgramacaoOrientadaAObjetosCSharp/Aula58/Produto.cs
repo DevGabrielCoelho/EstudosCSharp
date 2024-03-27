de12dc06366260e182fd8cbc8dba60dea15f45ec
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-03-27 16:43:36 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula58/Produto.cs
@@ -0,0 +1,88 @@
+﻿using System.Globalization;
+
+namespace Aula58
+{
+    class Produto
+    {
+        /* COMO ORGANIZAR UMA CLASSE:
+         * ATRIBUTOS PRIVADOS
+         * PROPRIEDADES AUTOIMPLEMENTADAS
+         * CONSTRUTORES
+         * PROPRIEDADES CUSTOMIZADAS
+         * OUTROS METODOS DA CLASSSE */
+
+        private string _nome;                                                   //                    //
+                                                                               // ATRIBUTOS PRIVADOS //
+                                                                              //                    //
+
+        //=======================================================================================================================================================//
+
+        public double Preco { get; private set; }                               //                 //
+                                                                               // AUTO PROPERTIES //
+                                                                              //                 //
+        public int Quantidade { get; private set; }
+
+        //=======================================================================================================================================================//
+
+        public Produto()                                                        //             //
+        {                                                                      // CONTRUTORES //
+            Preco = 5;                                                        //             //
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco, int quantidade)  
+        {                                                          
+            _nome = nome;                                        
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Produto(string nome, double preco)    
+        {                                          
+            _nome = nome;                         
+            Preco = preco;
+        }
+        public Produto(string nome) : this()                 
+        {                                                  
+            _nome = nome;                                 
+        }
+
+        //=======================================================================================================================================================//
+
+        public string Nome                                                      //                   //
+        {                                                                      // CUSTOM PROPERTIES //
+            get { return _nome; }                                             //                   //
+            set
+            {                                         
+                if (value != null && value.Length > 1)
+                {                                     
+                    _nome = value;
+                }
+            }
+        }
+
+        //=======================================================================================================================================================//
+
+        public double ValorTotalEmEstoque()                                     //                          //
+        {                                                                      // OUTROS METODOS DE CLASSE //
+            return Preco * Quantidade;                                        //                          //
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()  
+        {                                  
+            return _nome                  
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula58/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 4000.00, 10);
+
+            produto.Nome = (Console.ReadLine());
+
+            Console.WriteLine($"{produto.Nome}");
+            Console.WriteLine($"{produto.Preco}");
+            Console.WriteLine($"{produto.Quantidade}");
+            Console.WriteLine(produto);
+        }
+    }
+}
