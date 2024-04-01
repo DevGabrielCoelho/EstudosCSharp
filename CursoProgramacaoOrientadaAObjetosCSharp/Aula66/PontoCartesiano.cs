207f59637eebd33bda8e9d90d16a96c640361641
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-01 09:40:42 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula66/PontoCartesiano.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula66
+{
+    struct PontoCartesiano
+    {
+        public double x, y;
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula66/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PontoCartesiano PontoCartesiano;
+            PontoCartesiano.x = double.Parse(Console.ReadLine());
+            PontoCartesiano.y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(PontoCartesiano);
+            PontoCartesiano = new PontoCartesiano();
+            Console.WriteLine(PontoCartesiano);
+        }
+    }
+}
