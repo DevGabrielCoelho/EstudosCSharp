8c99f7ae4fbf30ea26f99041fd0d71547c8bad20
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-01 10:39:09 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula69/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade = int.Parse(Console.ReadLine());
+            double[] alturas = new double[quantidade];
+            double media = 0.0;
+            for (int i = 0; i < quantidade; i++)
+            {
+                alturas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                media += alturas[i];
+            }
+            media/= (double)quantidade;
+            Console.WriteLine($"Altura média: {media.ToString("f2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
