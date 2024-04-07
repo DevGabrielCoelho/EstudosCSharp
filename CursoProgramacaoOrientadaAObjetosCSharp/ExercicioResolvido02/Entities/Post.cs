a53745914670e639ff0f2c2513feb5bb3eb8508c
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-07 07:50:31 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioResolvido02/Entities/Post.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using ExercicioResolvido02.Entities;
+
+namespace ExercicioResolvido02.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComments(Comment comments)
+        {
+            Comments.Add(comments);
+        }
+        public void RemoveComments(Comment comments)
+        {
+            Comments.Remove(comments);
+        }
+
+    }
+}
--- CursoProgramacaoOrientadaAObjetosCSharp/ExercicioResolvido02/Program.cs
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioResolvido02/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using ExercicioResolvido02.Entities;
+
+namespace ExercicioResolvido02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Post> posts = new List<Post>()
+            {
+                new Post
+                (
+                    DateTime.Parse("21/06/2018 13:05:44"),
+                    "Traveling to New Zealand",
+                    "I'm going to visit this wonderful country!",
+                    12
+                ),
+                new Post
+                (
+                    DateTime.Parse("28/07/2018 23:14:19"),
+                    "Good night guys",
+                    "See you tomorrow",
+                    5
+                )
+            };
+
+            posts[0].Comments.AddRange(new Comment[] { new Comment("Have a nice trip"), new Comment("Wow that's awesome!") });
+            posts[1].Comments.AddRange(new Comment[] { new Comment("Good night"), new Comment("May the Force be with you") });
+
+            foreach (Post posted in posts)
+            {
+                Console.WriteLine($"{posted.Title}\n" +
+                                  $"{posted.Likes} - {posted.Moment}\n" +
+                                  $"{posted.Content}\n" +
+                                  $"Comments:");
+                foreach (Comment commented in posted.Comments)
+                {
+                    Console.WriteLine(commented.Text);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
