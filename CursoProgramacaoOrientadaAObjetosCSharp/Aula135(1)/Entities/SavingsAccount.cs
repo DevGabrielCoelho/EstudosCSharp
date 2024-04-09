c1fc2560d22e15e6b05c872f6e043f3251308b57
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-04-09 08:02:46 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula135(1)/Entities/SavingsAccount.cs
@@ -0,0 +1,18 @@
+﻿using Aula135.Entities;
+
+namespace Aula135_1_.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; private set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base (number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+    }
+}
