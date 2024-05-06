602c744ecd9cad01e69873c71173400b6ac4723b
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-05-06 10:12:26 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula197/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (FileStream fileStream = new(@"C:\Estudos\CursoProgramacaoOrientadaAObjetosCSharp\Aula197\TextTest01.txt", FileMode.Open))
+                {
+                    using (StreamReader streamReader = new(fileStream))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            Console.WriteLine(streamReader.ReadLine());
+                        }
+                    }
+                    Console.WriteLine();
+                    using (StreamReader streamReader = File.OpenText(@"C:\Estudos\CursoProgramacaoOrientadaAObjetosCSharp\Aula197\TextTest01.txt"))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            Console.WriteLine(streamReader.ReadLine());
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error! {ex.Message}");
+            }
+        }
+    }
+}
