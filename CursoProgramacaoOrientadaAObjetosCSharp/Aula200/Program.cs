7a28799cb9011458c32c337c9749325870b4ce41
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-05-06 11:12:38 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula200/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Estudos\CursoProgramacaoOrientadaAObjetosCSharp\Aula200\MyFolder\TestText01.txt";
+            Console.WriteLine($"DirectorySeparatorChar: {Path.DirectorySeparatorChar}\n" +
+                              $"PathSeparator: {Path.PathSeparator}\n" +
+                              $"GetDirectoryName: {Path.GetDirectoryName(path)}\n" +
+                              $"GetFileName: {Path.GetFileName(path)}\n" +
+                              $"GetExtension: {Path.GetExtension(path)}\n" +
+                              $"GetFileNameWithoutExtension: {Path.GetFileNameWithoutExtension(path)}\n" +
+                              $"GetFullPath: {Path.GetFullPath(path)}\n" +
+                              $"GetTempPath: {Path.GetTempPath()}");
+        }
+    }
+}
