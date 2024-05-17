ddd64d37d17160797178b4261d13392c0bf9a24a
Gabriel Coelho de Sousa Santos <nubsgpbr@hotmail.com>
2024-05-17 12:31:08 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioResolvido04/Entities/Log.cs
@@ -0,0 +1,32 @@
+﻿namespace Entities
+{
+    internal class Log
+    {
+        public string Name { get; private set; }
+        public HashSet<DateTime> Logon {  get; private set; } = new HashSet<DateTime>();
+
+        public Log(string name, DateTime log)
+        {
+            Name = name;
+            Logon.Add(log);
+        }
+        public Log(string name)
+        {
+            Name = name;
+        }
+        public void AddLog(DateTime log)
+        {
+            Logon.Add(log);
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Log)) return false;
+            Log other = obj as Log;
+            return Name == other.Name;
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/ExercicioResolvido04/Program.cs
@@ -0,0 +1,51 @@
+﻿using Entities;
+using System;
+using System.Formats.Asn1;
+
+namespace ExercicioResolvido04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = null;
+            StreamReader streamReader = null;
+            try
+            {
+                Console.Write("Enter file full path: ");
+                fileStream = new FileInfo(Console.ReadLine()).OpenRead();
+                HashSet<Log> logs = new HashSet<Log>();
+                streamReader = new StreamReader(fileStream);
+                while (!streamReader.EndOfStream)
+                {
+                    string[] lines = streamReader.ReadLine().Split(' ');
+                    DateTime dateTime = DateTime.Parse(lines[1]);
+                    if (logs.Contains(new Log(lines[0])))
+                    {
+                        Log[] logs1 = logs.ToArray();
+                        for (int i = 0; i < logs.Count; i++)
+                        {
+                            if (logs1[i].Name == lines[0])
+                            {
+                                logs1[i].AddLog(DateTime.Parse(lines[1]));
+                                break;
+                            }
+                        }
+                        logs = logs1.ToHashSet();
+                    }
+                    else logs.Add(new Log(lines[0], dateTime));
+                }
+                Console.WriteLine("Total users: " + logs.Count);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error! " + ex.Message + "!");
+            }
+            finally
+            {
+                fileStream.Close();
+                streamReader.Close();
+            }
+        }
+    }
+}
