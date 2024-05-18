5b9ec31fa606121cf7a9c3d2e160bb4d5e01121d
GabrielCoelhoDeSousaSantos <nubsgpbr@gmail.com>
2024-05-18 13:16:04 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula226B/Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    internal static class StringExtension
+    {
+        public static string Cut(this String thisObj, int range)
+        {
+            if (thisObj.Count() <= range) return thisObj;
+            else return thisObj.Substring(0, range) + "...";
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula226B/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula226B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, world!".Cut(5));
+        }
+    }
+}
