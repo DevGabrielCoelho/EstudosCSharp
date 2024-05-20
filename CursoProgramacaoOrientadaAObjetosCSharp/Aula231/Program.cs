f9626f4efa703f22d1a4ec9b5390975138cb411d
GabrielCoelhoDeSousaSantos <nubsgpbr@gmail.com>
2024-05-20 12:32:50 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula231/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Aula231.Services;
+
+namespace Aula231
+{
+    delegate void BinaryCalc(double x, double y);
+    delegate void UnitaryCalc(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryCalc calc = CalcService.Max;
+            calc += CalcService.Sum;
+            UnitaryCalc calc1 = CalcService.Pow;
+            calc1 += CalcService.Sqrt;
+
+            calc(4, 16);
+            calc1(4);
+            calc1(16);
+        }
+    }
+}
---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula231/Services/CalcService.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula231.Services
+{
+    internal class CalcService
+    {
+        public static void Max(double x, double y)
+        {
+            Console.WriteLine((x > y) ? x : y);
+        }
+
+        public static void Sum(double x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        public static void Pow(double x)
+        {
+            Console.WriteLine(x * x);
+        }
+        
+        public static void Sqrt(double x)
+        {
+            Console.WriteLine(Math.Sqrt(x));
+        }
+    }
+}
