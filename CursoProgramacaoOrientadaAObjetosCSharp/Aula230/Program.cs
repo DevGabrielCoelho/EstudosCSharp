cf5f33afa1d615f71ebc6b295f653727a3616fe4
GabrielCoelhoDeSousaSantos <nubsgpbr@gmail.com>
2024-05-20 12:03:41 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula230/Program.cs
@@ -0,0 +1,25 @@
+﻿using Aula230.Services;
+using System;
+
+namespace Aula230
+{
+    delegate double BynaryCalc(double n1, double n2);
+    delegate double UnaryCalc(double n1);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 2.0;
+            double y = 3.0;
+
+            BynaryCalc sum = CalcService.Sum;
+            BynaryCalc max = CalcService.Max;
+            UnaryCalc square = CalcService.Square;
+
+            Console.WriteLine($"{sum(x, y)}\n" +
+                              $"{max(x,y)}\n" +
+                              $"{square(x)}\n" +
+                              $"{square(y)}");
+        }
+    }
+}
