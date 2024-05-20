fb714072d06d16cb4c9efad3b9228fcbcd3bf510
GabrielCoelhoDeSousaSantos <nubsgpbr@gmail.com>
2024-05-20 09:06:11 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula228/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aula228.Entities;
+using System;
+
+namespace Aula228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("TV", 2000.00));
+            products.Add(new Product("CELL", 3000.00));
+            products.Add(new Product("PC", 4000.00));
+
+            products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
