43560c9a250eef2122246c46628965ab9e899a15
GabrielCoelhoDeSousaSantos <nubsgpbr@gmail.com>
2024-05-28 11:03:35 -0300

    estudos

---
+++ CursoProgramacaoOrientadaAObjetosCSharp/Aula235/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Aula235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5, };
+
+            IEnumerable<int> Even = ints.Where(x => x % 2 == 0);
+            IEnumerable<int> Odd = ints.Where(x => x % 2 != 0);
+
+            foreach (int x in Even)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            foreach (int x in Odd)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
